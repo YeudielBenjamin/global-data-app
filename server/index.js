fe0d44a3c060d14f8e62a512ce136df316a983b7
YeudielBenjamin <yeudiel.vazquez@alumnos.udg.mx>
2017-05-08 22:05:51 -0500

    configuración del servidor

---
+++ server/index.js
@@ -0,0 +1,23 @@
+'use strict'
+
+var mongoose = require("mongoose");
+var app      = require("./app");
+
+var port = process.env.PORT || 3000;
+
+mongoose.connect("mongodb://localhost:27017/global-data-app", (err, res) => {
+    if(err){
+        console.log("Sucedió un error al conectar a la base de datos");
+        throw err;
+    }
+    else{
+        app.listen(port, (err, res) => {
+            if (err) {
+                console.log("Sucedió un error mientras se cargaba la aplicación");
+                throw err;
+            } else {
+                console.log("Aplicación corriendo en http://localhost:" + port);
+            }
+        });
+    }
+});
