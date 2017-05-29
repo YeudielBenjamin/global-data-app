23fa8eb7619e8fb379e7ddaedd8ee2802a36ad00
YeudielBenjamin <yeudiel.vazquez@alumnos.udg.mx>
2017-05-29 01:52:24 -0500

    Display timeline

--- client/src/app/timeline/timeline.component.ts
+++ client/src/app/timeline/timeline.component.ts
@@ -13,6 +13,10 @@ export class TimelineComponent implements OnInit {
   public timelineWidth = 550;
   public timelineHeight = 180;
   public timelineMargins = {left:40,top:10,bottom:5,right:80};
+  public yearsDomain = [1970, 2012];
+  public yearScale;
+  public yearAxis;
+  public timelineAxisGroup;
 
   constructor() { }
 
@@ -31,6 +35,24 @@ export class TimelineComponent implements OnInit {
         .attr("transform","translate("+this.timelineMargins.left+","+this.timelineMargins.top+")");
 
     $("#timeline svg").attr("height", (this.timelineHeight + this.timelineMargins.top + this.timelineMargins.bottom));
+
+    this.timelineAxisGroup = this.timeline.append("g")
+      .attr("class", "timeAxis")
+      .attr("transform", "translate(0,"+(this.timelineHeight - 30)+")");
+
+    this.yearScale = d3.scalePow()
+      .domain(this.yearsDomain);
+
+    this.yearScale = d3.scaleLinear().range([0, this.timelineWidth]);
+    this.yearScale.domain(this.yearsDomain);
+
+    this.yearAxis = d3.axisBottom(this.yearScale)
+      .ticks(this.timelineWidth / 70) // Algo tiene que ver con los números de abajo :v
+      .tickSize(10/*, 5, this.timelineHeight*/) // Tamaño de la línea ↨
+      //.tickSubdivide(4)
+      .tickPadding(5); // De la línea al número ↨
+
+    this.timelineAxisGroup.call(this.yearAxis);
 
   }
 
